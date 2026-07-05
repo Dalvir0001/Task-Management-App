0a4d3f37d56b518bb00ed51bd83cb8cc1e81e8ce
Dalvir Singh <dalvir182600@gmail.com>
2026-07-05 23:55:10 +0530

    Complete task CRUD APIs with JWT authorization

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -1,5 +1,20 @@
 const Task = require("../models/task");
 
+const getTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find({
+            user: req.user.id,
+        });
+
+        res.status(200).json(tasks);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
 const createTask = async (req, res) => {
     try {
         const { title, description } = req.body;
@@ -24,7 +39,71 @@ const createTask = async (req, res) => {
         });
     }
 };
+const updateTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found",
+            });
+        }
+
+        if (task.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                message: "Not authorized",
+            });
+        }
+
+        const updatedTask = await Task.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {
+                new: true,
+            }
+        );
+
+        res.status(200).json(updatedTask);
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
+const deleteTask = async (req, res) => {
+    try {
+        const task = await Task.findById(req.params.id);
+
+        if (!task) {
+            return res.status(404).json({
+                message: "Task not found",
+            });
+        }
+
+        if (task.user.toString() !== req.user.id) {
+            return res.status(401).json({
+                message: "Not authorized",
+            });
+        }
+
+        await Task.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({
+            message: "Task deleted successfully",
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
 
 module.exports = {
     createTask,
+    getTasks,
+    updateTask,
+    deleteTask,
 };
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -2,8 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middleware/authMiddleware");
-const { createTask } = require("../controllers/taskController");
+const {
+    createTask,
+    getTasks,
+    updateTask,
+    deleteTask,
+} = require("../controllers/taskController");
 
 router.post("/", protect, createTask);
+router.get("/", protect, getTasks);
+router.put("/:id", protect, updateTask);
+router.delete("/:id", protect, deleteTask);
 
 module.exports = router;
